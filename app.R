3a256ce95d43db80857991484d67345fa09ef4e5
Pierre R Vernier <pierre.vernier@gmail.com>
2024-08-03 13:20:17 -0700

    Update app.R

--- app.R
+++ app.R
@@ -1,69 +1,54 @@
 library(shiny)
 library(bslib)
 
+# Define UI for app that draws a histogram ----
 ui <- page_sidebar(
 
-  title = "Tabsets",
+  # App title ----
+  title = "Hello Shiny!",
+
+  # Sidebar panel for inputs ----
   sidebar = sidebar(
-    radioButtons(
-      "dist",
-      "Distribution type:",
-      c(
-        "Normal" = "norm",
-        "Uniform" = "unif",
-        "Log-normal" = "lnorm",
-        "Exponential" = "exp"
-      )
-    ),
-    br(),
+
+    # Input: Slider for the number of bins ----
     sliderInput(
-      "n",
-      "Number of observations:",
-      value = 500,
+      inputId = "bins",
+      label = "Number of bins:",
       min = 1,
-      max = 1000
+      max = 50,
+      value = 30
     )
   ),
 
-  navset_card_underline(
-    nav_panel("Plot", plotOutput("plot")),
-    nav_panel("Summary", verbatimTextOutput("summary")),
-    nav_panel("Table", tableOutput("table"))
-  )
+  # Output: Histogram ----
+  plotOutput(outputId = "distPlot")
 )
 
+# Define server logic required to draw a histogram ----
 server <- function(input, output) {
 
-  d <- reactive({
-    dist <- switch(
-      input$dist,
-      norm = rnorm,
-      unif = runif,
-      lnorm = rlnorm,
-      exp = rexp,
-      rnorm
-    )
-    dist(input$n)
-  })
+  # Histogram of the Old Faithful Geyser Data ----
+  # with requested number of bins
+  # This expression that generates a histogram is wrapped in a call
+  # to renderPlot to indicate that:
+  #
+  # 1. It is "reactive" and therefore should be automatically
+  #    re-executed when inputs (input$bins) change
+  # 2. Its output type is a plot
+  output$distPlot <- renderPlot({
+    x <- faithful$waiting
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-  output$plot <- renderPlot({
-    dist <- input$dist
-    n <- input$n
     hist(
-      d(),
-      main = paste("r", dist, "(", n, ")", sep = ""),
+      x,
+      breaks = bins,
       col = "#75AADB",
-      border = "white"
+      border = "white",
+      xlab = "Waiting time to next eruption (in mins)",
+      main = "Histogram of waiting times"
     )
-  })
-
-  output$summary <- renderPrint({
-    summary(d())
-  })
-
-  output$table <- renderTable({
-    d()
   })
 }
 
-shinyApp(ui, server)
+# Create Shiny app ----
+shinyApp(ui = ui, server = server)
